976b8136eb1a1e751563f09d4efe2f185e8afc8f
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-17 20:25:40 +0900

    Homeコンポーネントを制作

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import "./Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContes">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+
+        <div className="postTextContainer">
+          今はreactの学習中です。これから頑張ってReactエンジニアとして活躍してきます。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@kakedaCode</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };

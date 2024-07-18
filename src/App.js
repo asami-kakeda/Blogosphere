f78aafd1ea294d89fd38ec1ee50e21edca1e4278
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-18 12:53:09 +0900

    リロードしても自動的にログインできるようにする

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useState } from "react";
 import Login from "./components/Login";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
   return (
     <Router>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { auth, db } from "../firebase";
 
 export const Home = () => {
   const [postList, setPostList] = useState([]);
@@ -31,7 +31,9 @@ export const Home = () => {
             <div className="postTextContainer">{post.postsText}</div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              <button onClick={() => handleDelete(post.id)}>削除</button>
+              {post.author.id === auth.currentUser?.uid && (
+                <button onClick={() => handleDelete(post.id)}>削除</button>
+              )}
             </div>
           </div>
         );

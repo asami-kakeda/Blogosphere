defa49bc68582eac3e5d5c9f57e06cd6a58fc198
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-18 10:55:01 +0900

    map関数を使用してデータを一つずつ取り出す

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 export const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContes">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContes" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
 
-        <div className="postTextContainer">
-          今はreactの学習中です。これから頑張ってReactエンジニアとして活躍してきます。
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@kakedaCode</h3>
-          <button>削除</button>
-        </div>
-      </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
